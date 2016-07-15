1409a2662a23da9602c7e60d52614986366540de
Berkan Dirim <berkandirim@Berkan-Mac.local>
2016-07-15 15:38:23 +0300

    Added files

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+var Card = React.createClass({
+    getInitialState: function() {
+        return {
+              
+        };
+    },
+    componentDidMount: function() {
+        var component = this;
+        $.get('https://api.github.com/users/' + this.props.login, function(data) {
+            component.setState(data);
+        });
+    },
+    render: function() {
+        return (
+            <div>
+                <img src={this.state.avatar_url} alt="Avatar" width="80" />
+                <h3>{this.state.name}</h3>
+                <hr />
+            </div>
+        )
+        
+    }
+});
+
+var Main = React.createClass({
+    getInitialState: function() {
+        return {
+            logins: ['zpao', 'fisherw']
+        };
+    },
+    render: function() {
+        return (
+            <div>
+                <Card login="berkandirim" />
+                <Card login="petehunt" />
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(<Main />, document.getElementById('root'));
